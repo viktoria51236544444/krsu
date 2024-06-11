f7610af1edeb047344324fd92d31b5c94e20ab90
Viktoria Bratskih <peace.kokos@gmail.com>
2024-06-11 13:05:03 +0600

    fggjjh

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 
 const HomePage = () => {
     return (
-        <Container className="home-container" >
+        <Container className="home-container">
             <Row className="justify-content-start align-items-start text-center">
                 <Col>
                     <h1 className="home-title">Добро пожаловать на портал Закупок КНАУ им. К.И. Скрябина!</h1>
@@ -14,24 +14,28 @@ const HomePage = () => {
             <Row className="justify-content-center align-items-center text-center">
                 <Col xs={12} md={6} className="order-md-last">
                     <Card className="map-card">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d78694.71539150541!2d74.48373497128125!3d42.862081595046654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb6301e663e2d%3A0x623be3ff09d65a63!2z0JrRi9GA0LPRi9C30YHQutC40Lkg0L3QsNGG0LjQvtC90LDQu9GM0L3Ri9C5INCw0LPRgNCw0YDQvdGL0Lkg0YPQvdC40LLQtdGA0YHQuNGC0LXRgg!5e0!3m2!1sru!2skg!4v1716933453453!5m2!1sru!2skg" width="635" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d78694.71539150541!2d74.48373497128125!3d42.862081595046654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389eb6301e663e2d%3A0x623be3ff09d65a63!2z0JrRi9GA0LPRi9C30YHQutC40Lkg0L3QsNGG0LjQvtC90LDQu9GM0L3Ri9C5INCw0LPRgNCw0YDQvdGL0Lkg0YPQvdC40LLQtdGA0YHQuNGC0LXRgg!5e0!3m2!1sru!2skg!4v1716933453453!5m2!1sru!2skg"
+                            allowFullScreen
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe>
                     </Card>
                 </Col>
                 <Col xs={12} md={6} className="order-md-first">
                     <Card className="description-card">
                         <Card.Body>
-                            <Card.Text><h6>Закупки Кыргызского национального аграрного университета им. К.И. Скрябина</h6></Card.Text>
+                            <Card.Text>
+                                <h6>Закупки Кыргызского национального аграрного университета им. К.И. Скрябина</h6>
+                            </Card.Text>
                             <Card.Text className='cardtext-p'>
                                 Здесь вы найдете всю необходимую информацию о текущих закупках и тендерах, проводимых университетом. Мы стремимся к прозрачности и эффективности в процессе закупок, чтобы обеспечить нашему университету необходимое оборудование, услуги и материалы для успешной научной и учебной деятельности.
                             </Card.Text>
                         </Card.Body>
                     </Card>
-                </Col> 
-
+                </Col>
             </Row>
-      
         </Container>
-        
     );
 };
 
